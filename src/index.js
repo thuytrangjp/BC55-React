c8f68cd97632a1b9073420d1f3f1bc07ed7e0117
Chan <107294488+thuytrangjp@users.noreply.github.com>
2023-11-12 19:06:11 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import './index.css';
 import App from './App';
@@ -18,3 +17,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+import 'bootstrap/dist/js/bootstrap.min';
